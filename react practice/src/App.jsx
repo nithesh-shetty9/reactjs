6c45df74668facb0bb4f9bc9fed720372ac438a0
Nithesh <nitheshnithesh310@gmail.com>
2026-07-18 19:52:46 +0530

    Add initial React application setup with components and assets
    - Created main application files: App.jsx, App2.jsx, and main.jsx
    - Added basic table structure in App component
    - Implemented App2 component with a button and heading
    - Included SVG icons in public/icons.svg
    - Added assets: hero.png, react.svg, and vite.svg
    - Created CSS files for styling: App.css and index.css
    - Configured Vite for React project in vite.config.js

---
+++ react practice/src/App.jsx
@@ -0,0 +1,14 @@
+export const App=function(){
+  return(
+  <>
+  <title>Mynithesh</title>
+    <table border="1">
+      <tr>
+      <th>name</th>
+      <th>Email</th>
+      <th>Phoneno:</th>
+      </tr>
+    </table>
+
+  </>);
+};
---
+++ react practice/src/App2.jsx
@@ -0,0 +1,8 @@
+export const App2 = () => {
+  return (
+    <div>
+      <button>Click me</button>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
