e3146ecf761a23ed1b718d8477fa3b0d6146a2f7
Nithesh <nitheshnithesh310@gmail.com>
2026-08-02 06:52:03 +0530

    46

--- react practice/src/components/Hooks.jsx
+++ react practice/src/components/Hooks.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 
 export const Practice=()=>{
     const [counter,setcount]=useState(0);
+    const [step,setstep]=useState("");
     return(
         <>
         <h1>Counter</h1>
         <h1>Count:{counter}</h1>
-        <button onClick={
-            ()=>setcount(counter+1)
-        }>ADD+</button>
-        <button onClick={()=>setcount(()=>counter-1>=0?counter-1:0)}>MINUS-</button>
+        <h2>step<input type="number" value={step} onChange={(event)=>{
+            if(event.target.value===""){
+                setstep("");
+            }else{
+                let temp = Number(event.target.value);
+                setstep(temp);
+            }
+        }}></input></h2>
+        <button onClick={() =>
+    setcount(prev => prev + (Number(step) > 0 ? Number(step) : 1))
+  }>ADD+</button>
+        <button onClick={()=>setcount(()=>counter-Number(step)>=0?counter-Number(step):0)}>MINUS-</button>
         <button onClick={()=>setcount(0)}>RESET</button>
         </>
     );
