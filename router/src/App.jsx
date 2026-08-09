8d771a9713897f8114c5072c9d08cd6d2f5b44bf
Nithesh <nitheshnithesh310@gmail.com>
2026-08-09 18:43:26 +0530

    feat: initialize router application with basic structure and components
    - Added main application file (App.jsx) with routing setup using react-router.
    - Created layout components (Header, Footer, Applayout) for consistent structure.
    - Implemented pages (Home, About, Movie, Contact) with basic content.
    - Included SVG icons for branding and UI elements.
    - Added CSS files for styling.
    - Configured Vite for development environment.

---
+++ router/src/App.jsx
@@ -0,0 +1,53 @@
+import {createBrowserRouter,RouterProvider }from "react-router";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Movie } from "./pages/Movie";
+import { Contact } from "./pages/Contact";
+import { Applayout } from "./components/layout/Applayout";
+
+const App=()=>{
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<Applayout/>,
+      children:[
+        {
+      path:"/",
+      element:<Home/>
+    },
+      {
+      path:"/about",
+      element:<About/>
+    },
+      {
+      path:"/movie",
+      element:<Movie/>
+    } , {
+      path:"/contact",
+      element:<Contact/>
+    }
+
+      ]
+    }
+  ]);
+  return <RouterProvider router={router}></RouterProvider>
+}
+export default App;
+//........................older method
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/movie" element={<Movie />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
---
+++ router/src/components/layout/Header.jsx
@@ -0,0 +1,5 @@
+export const Header=()=>{
+    return(
+        <h1>HEADER</h1>
+    )
+}
